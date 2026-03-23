702a67af0a4a1c697fb94d927636eb9176bb41ac
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-23 17:02:15 +0000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -80,24 +80,22 @@ function checkMobileMode() {
 checkMobileMode();
 window.addEventListener("resize", checkMobileMode);
 function spawnDust() {
-    if (mode === "terminal"){
-        const rect = title.getBoundingClientRect();
-        const startY = rect.bottom - 4;
+    const rect = title.getBoundingClientRect();
+    const startY = rect.bottom - 4;
 
-        for (let i = 0; i < 120; i++) {
-            setTimeout(() => {
-                const dust = document.createElement("div");
-                dust.className = "dust";
-                dust.style.left = rect.left + Math.random() * rect.width + "px";
-                dust.style.top = startY + "px";
-                const x = (Math.random() - 0.5) * 40;
-                const y = 180 + Math.random() * 80;
-                dust.style.setProperty("--x", x + "px");
-                dust.style.setProperty("--y", y + "px");
-                document.body.appendChild(dust);
-                setTimeout(() => dust.remove(), 2600);
-            }, Math.random() * 350);
-        }
+    for (let i = 0; i < 120; i++) {
+        setTimeout(() => {
+            const dust = document.createElement("div");
+            dust.className = "dust";
+            dust.style.left = rect.left + Math.random() * rect.width + "px";
+            dust.style.top = startY + "px";
+            const x = (Math.random() - 0.5) * 40;
+            const y = 180 + Math.random() * 80;
+            dust.style.setProperty("--x", x + "px");
+            dust.style.setProperty("--y", y + "px");
+            document.body.appendChild(dust);
+            setTimeout(() => dust.remove(), 2600);
+        }, Math.random() * 350);
     }
 }
 
