9001509e5cfeac0c6c8aa59cd6a0a9d1047a03f9
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-28 12:12:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ CUSTOM_DIR = {
         "site": "/cow-servers/",
         "cow-servers.github": "/repo/cow_servers"
     },
-    "github_repos/": {}
+    "github_repods/": {}
 }
 
 def get_current_folder():
