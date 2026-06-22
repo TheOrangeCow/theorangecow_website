1b5e21d8a2e37b53c69d84dca2296ea32ba194fb
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-06-22 19:48:35 +0100

    update

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def index():
 
 @app.route("/library")
 def index():
-    return render_template("index.html")
+    return render_template("library.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
